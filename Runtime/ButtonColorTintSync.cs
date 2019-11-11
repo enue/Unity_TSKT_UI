1d0d7737025059ec9d56ae5260638f9867767976
enue <nnp@car.ocn.ne.jp>
2019-11-12 06:39:04 +0900

    0.14.0 ButtonColorTintSyncのpressedColor, selectedColor, highlighedColor対応

--- Runtime/ButtonColorTintSync.cs
+++ Runtime/ButtonColorTintSync.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace TSKT
 {
     [RequireComponent(typeof(Button))]
-    public class ButtonColorTintSync : MonoBehaviour
+    public class ButtonColorTintSync : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
     {
         Button button;
         Button Button => button ?? (button = GetComponent<Button>());
 
         Color currentColor = Color.white;
+        bool isPointerDown = false;
+        bool isPointerInside = false;
+        bool hasSelect = false;
 
         [SerializeField]
         Graphic[] targets = default;
@@ -19,16 +23,69 @@ namespace TSKT
         void Update()
         {
             var colorBlock = Button.colors;
-            var color = Button.interactable ? colorBlock.normalColor : colorBlock.disabledColor;
+            Color color;
+            var tweenInstant = false;
+            if (!Button.interactable)
+            {
+                color = colorBlock.disabledColor;
+                tweenInstant = true;
+            }
+            else if (isPointerDown)
+            {
+                color = colorBlock.pressedColor;
+            }
+            else if (hasSelect)
+            {
+                color = colorBlock.selectedColor;
+            }
+            else if (isPointerInside)
+            {
+                color = colorBlock.highlightedColor;
+            }
+            else
+            {
+                color = colorBlock.normalColor;
+            }
+
             if (currentColor != color)
             {
-                var fadeDuration = colorBlock.fadeDuration;
+                var fadeDuration = tweenInstant ? 0f : colorBlock.fadeDuration;
                 currentColor = color;
                 foreach (var target in targets)
                 {
                     target.CrossFadeColor(currentColor, fadeDuration, true, true);
                 }
             }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            isPointerDown = false;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            isPointerDown = true;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isPointerInside = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isPointerInside = false;
+        }
+
+        public void OnSelect(BaseEventData eventData)
+        {
+            hasSelect = true;
+        }
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            hasSelect = false;
         }
     }
 }
