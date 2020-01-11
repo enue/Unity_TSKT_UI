37644da6c5d57c565e5d9d47c86328c365fc56fd
enue <nnp@car.ocn.ne.jp>
2020-01-11 14:50:31 +0900

    VerticalLayoutGroupLite : 横方向位置の整列処理

--- Runtime/VerticalLayoutGroupLite.cs
+++ Runtime/VerticalLayoutGroupLite.cs
@@ -11,6 +11,13 @@ namespace TSKT
     [RequireComponent(typeof(RectTransform))]
     public class VerticalLayoutGroupLite : UIBehaviour, ILayoutElement, ILayoutGroup
     {
+        public enum ChildAlignment
+        {
+            Left = 0,
+            Center = 1,
+            Right = 2
+        }
+
         RectTransform rectTransform;
         RectTransform RectTransform => rectTransform ?? (rectTransform = (RectTransform)transform);
 
@@ -24,7 +31,16 @@ namespace TSKT
         float paddingBottom = 0f;
 
         [SerializeField]
+        float paddingLeft = 0f;
+
+        [SerializeField]
+        float paddingRight = 0f;
+
+        [SerializeField]
         float spacing = 0f;
+
+        [SerializeField]
+        ChildAlignment childAlignment = ChildAlignment.Center;
 
         public float minWidth => -1f;
         public float preferredWidth => -1f;
@@ -76,6 +92,11 @@ namespace TSKT
             var pivot = rectTransform.pivot;
             var top = (1f - pivot.y) * preferredHeight - paddingTop;
 
+            var rect = rectTransform.rect;
+            var left = -pivot.x * rect.width + paddingLeft;
+            var right = (1f - pivot.x) * rect.width - paddingRight;
+            var center = (left + right) / 2f;
+
             var index = 0;
             foreach (RectTransform child in transform)
             {
@@ -84,7 +105,27 @@ namespace TSKT
                     continue;
                 }
                 var pos = child.localPosition;
-                pos.y = top - index * (childHeight + spacing) - childHeight * (1f - child.pivot.y);
+                var childPivot = child.pivot;
+                var childWidth = child.rect.width;
+
+                switch (childAlignment)
+                {
+                    case ChildAlignment.Left:
+
+                        pos.x = left + childWidth * childPivot.x;
+                        break;
+                    case ChildAlignment.Center:
+                        pos.x = center + childWidth * (0.5f - childPivot.x);
+                        break;
+                    case ChildAlignment.Right:
+                        pos.x = right - childWidth * (1f - childPivot.x);
+                        break;
+                    default:
+                        Debug.LogError("unknown alignment : " + childAlignment.ToString());
+                        break;
+                }
+
+                pos.y = top - index * (childHeight + spacing) - childHeight * (1f - childPivot.y);
                 child.localPosition = pos;
                 ++index;
             }
