a94651891d9d3e8eafbad92e7342720eccb8d33d
enue <nnp@car.ocn.ne.jp>
2020-01-14 18:06:09 +0900

    0.16.4 : FullScreenButtonのウィンドウモード時の解像度選択処理を修正

--- Runtime/FullScreenButton.cs
+++ Runtime/FullScreenButton.cs
@@ -7,58 +7,59 @@ namespace TSKT
 {
     public class FullScreenButton : MonoBehaviour
     {
-        [SerializeField]
-        Vector2Int aspectRatio = new Vector2Int(16, 9);
-
-        Vector2Int windowedSize;
-
         public void OnClicked()
         {
             if (Screen.fullScreen)
             {
-                if (windowedSize.sqrMagnitude == 0)
-                {
-                    // 使える解像度のうち2番目に大きいものを選ぶ
-                    windowedSize = GetSizes()
-                        .Distinct()
-                        .OrderByDescending(_ => _.x)
-                        .Skip(1)
-                        .DefaultIfEmpty(new Vector2Int(Screen.width, Screen.height))
-                        .First();
-                }
+                var windowedSize = SuggestWindowedSize();
                 Screen.SetResolution(windowedSize.x, windowedSize.y, fullscreen: false);
+
+#if UNITY_EDITOR
+                Debug.Log("windowedSize : " + windowedSize);
+#endif
             }
             else
             {
-                windowedSize = new Vector2Int(Screen.width, Screen.height);
-
-                // 使える解像度のうち一番大きいものを選ぶ
-                var res = GetSizes()
-                    .Distinct()
-                    .OrderByDescending(_ => _.x)
-                    .First();
-
+                var res = SuggestFullScreenSize();
                 Screen.SetResolution(res.x, res.y, fullscreen: true);
+
+#if UNITY_EDITOR
+                Debug.Log("fullScreenResolution : " + res);
+#endif
             }
         }
 
-        List<Vector2Int> GetSizes()
+        static public Vector2Int SuggestWindowedSize()
         {
-            var resolutions = new List<Vector2Int>();
-            foreach (var it in Screen.resolutions)
-            {
-                if (it.width * aspectRatio.y > it.height * aspectRatio.x)
-                {
-                    // 横長ディスプレイは縦をあわせる
-                    resolutions.Add(new Vector2Int(it.height * aspectRatio.x / aspectRatio.y, it.height));
-                }
-                else
-                {
-                    // 縦長ディスプレイは横をあわせる
-                    resolutions.Add(new Vector2Int(it.width, it.width * aspectRatio.y / aspectRatio.x));
-                }
-            }
-            return resolutions;
+            // 最大サイズの2/3以下の解像度を選ぶ
+            // モニタが1920x1080ならウィンドウサイズ1280x720が選ばれる想定
+            var res = GetResolutions();
+            var w = res.Max(_ => _.x) * 2 / 3;
+            var h = res.Max(_ => _.y) * 2 / 3;
+
+            return res.Where(_ => _.x <= w)
+                .Where(_ => _.y <= h)
+                .OrderByDescending(_ => _.x)
+                .ThenByDescending(_ => _.y)
+                .DefaultIfEmpty(new Vector2Int(Screen.width, Screen.height))
+                .First();
+        }
+
+        static public Vector2Int SuggestFullScreenSize()
+        {
+            // 使える解像度のうち一番大きいものを選ぶ
+            return GetResolutions()
+                .OrderByDescending(_ => _.x)
+                .ThenByDescending(_ => _.y)
+                .First();
+        }
+
+        static public Vector2Int[] GetResolutions()
+        {
+            return Screen.resolutions
+                .Select(_ => new Vector2Int(_.width, _.height))
+                .Distinct()
+                .ToArray();
         }
     }
 }
