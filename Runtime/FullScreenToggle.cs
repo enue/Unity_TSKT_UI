f7b7dd7aaf1c7640a9cc1d88e211c67913b75fef
enu <otoriden@gmail.com>
2020-05-23 07:43:58 +0900

    0.17.4 FullscreenToggle

---
+++ Runtime/FullScreenToggle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace TSKT
+{
+    public class FullScreenToggle : MonoBehaviour
+    {
+        void Start()
+        {
+            var toggle = GetComponent<Toggle>();
+            if (toggle)
+            {
+                toggle.SetIsOnWithoutNotify(Screen.fullScreen);
+            }
+        }
+
+        public void OnValueChanged(bool value)
+        {
+            if (value)
+            {
+                var res = FullScreenButton.SuggestFullScreenSize();
+                Screen.SetResolution(res.x, res.y, fullscreen: true);
+            }
+            else
+            {
+                var windowedSize = FullScreenButton.SuggestWindowedSize();
+                Screen.SetResolution(windowedSize.x, windowedSize.y, fullscreen: false);
+            }
+        }
+    }
+}
