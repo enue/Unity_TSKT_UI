964fa56d8293a2a229bcdf369f8768ba658bbae7
enue <otoriden@gmail.com>
2022-04-09 10:21:05 +0900

    0.20.13 TextMeshProUtil.SetText

---
+++ Assets/Package/Runtime/TextMeshProUtil.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+#if TSKT_UI_SUPPORT_TEXTMESHPRO
+namespace TSKT
+{
+    public class TextMeshProUtil
+    {
+        // テクスチャがいっぱいになった時にFontAsset.ClearFontAssetDataを呼ぶ
+        public static void SetText(TMPro.TMP_Text target, string value)
+        {
+            if (target.font.TryAddCharacters(value))
+            {
+                target.SetText(value);
+            }
+            else
+            {
+                var targetTexts = Object.FindObjectsOfType<TMPro.TMP_Text>(includeInactive: true)
+                    .Where(_ => _ != target && _.font == target.font)
+                    .Select(_ => (label: _, text: _.text))
+                    .ToArray();
+                target.font.ClearFontAssetData();
+                target.SetText(value);
+                foreach (var (label, text) in targetTexts)
+                {
+                    label.SetText(text);
+                }
+            }
+        }
+    }
+}
+#endif
