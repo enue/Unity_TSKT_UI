f0c3a02b29133812f062a59d408a5faf2fefe00f
enue <otoriden@gmail.com>
2022-06-17 20:56:21 +0900

    tmp_layoutmaxsize

---
+++ Assets/Package/Runtime/TMP_LayoutMaxSize.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+#nullable enable
+
+namespace TSKT
+{
+#if TSKT_UI_SUPPORT_TEXTMESHPRO
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(TMPro.TMP_Text))]
+    public class TMP_LayoutMaxSize : MonoBehaviour, ILayoutElement
+    {
+        TMPro.TMP_Text? text;
+        TMPro.TMP_Text Text => text ? text! : (text = GetComponent<TMPro.TMP_Text>());
+
+        [SerializeField]
+        float maxWidth = -1;
+
+        [SerializeField]
+        float maxHeight = -1;
+
+        public float flexibleHeight => -1f;
+        public float flexibleWidth => -1f;
+        public int layoutPriority => 1;
+        public float minHeight => -1f;
+        public float minWidth => -1f;
+
+        public float preferredHeight
+        {
+            get
+            {
+                if (maxHeight < 0f)
+                {
+                    return Text.preferredHeight;
+                }
+                return Mathf.Min(Text.preferredHeight, maxHeight);
+            }
+            set
+            {
+                maxHeight = value;
+                SetDirty();
+            }
+        }
+        public float preferredWidth
+        {
+            get
+            {
+                if (maxWidth < 0f)
+                {
+                    return Text.preferredWidth;
+                }
+                return Mathf.Min(Text.preferredWidth, maxWidth);
+            }
+            set
+            {
+                maxWidth = value;
+                SetDirty();
+            }
+        }
+        public void CalculateLayoutInputHorizontal() { }
+        public void CalculateLayoutInputVertical() { }
+
+        protected void SetDirty()
+        {
+            if (!gameObject.activeInHierarchy)
+            {
+                return;
+            }
+            LayoutRebuilder.MarkLayoutForRebuild(transform as RectTransform);
+        }
+
+#if UNITY_EDITOR
+        protected void OnValidate()
+        {
+            SetDirty();
+        }
+#endif
+    }
+#else
+    [DisallowMultipleComponent]
+    public class TMP_LayoutMaxSize : MonoBehaviour
+    {
+    }
+#endif
+}
+
+
